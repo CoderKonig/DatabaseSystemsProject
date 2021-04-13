dfb063f7deb3bb2f79a026ccae4b66bd88d72766
Inesh Raj <47544380+CoderKonig@users.noreply.github.com>
2021-04-13 02:16:05 -0400

    DBProject skeleton

---
+++ carRentalService/Model/carRentalWeb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace carRentalService.Model
+{
+    public class carRentalWeb
+    {
+       public string ConnectionString { get; set; }
+        public carRentalWeb (string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ carRentalService/Model/table_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace carRentalService.Model
+{
+    public class table_1
+    {
+       
+        private carRentalWeb context;
+        [DataType(DataType.Date)]
+        public int insurane_id { get; set; }
+        public int customer_id { get; set; }
+        public string insurance_number { get; set; }
+        public string company { get; set; }
+        public string insurance_phone { get; set; }
+
+
+
+    }
+}
